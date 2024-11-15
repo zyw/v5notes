3981b285643afc3a49333bb47e12628fee9ad250
zyw <zyw090111@163.com>
2024-11-15 22:17:03 +0800

    修改使用IPv6的bug

--- v5notes/script/sql/notes.sql
+++ v5notes/script/sql/notes.sql
@@ -78,3 +78,15 @@ values(1849657043265904646, '笔记删除', 1849657043265904642, '4',  '#', '', 
 insert into sys_menu (menu_id, menu_name, parent_id, order_num, path, component, is_frame, is_cache, menu_type, visible, status, perms, icon, create_dept, create_by, create_time, update_by, update_time, remark)
 values(1849657043265904647, '笔记导出', 1849657043265904642, '5',  '#', '', 1, 0, 'F', '0', '0', 'notes:notes:export',       '#', 103, 1, sysdate(), null, null, '');
 
+
+-- 客户端表
+INSERT INTO `sys_client` (`id`, `client_id`, `client_key`, `client_secret`, `grant_type`, `device_type`, `active_timeout`, `timeout`, `status`, `del_flag`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`) VALUES (1851635079091687426, 'd9df3c0228c05e5c61db46348bcadf6e', 'client', 'client123', 'clientPassword', 'pc_client', -1, 604800, '0', '0', 103, 1, '2024-10-30 22:39:39', 1, '2024-11-10 16:32:44');
+
+-- 字典类型表
+INSERT INTO `sys_dict_type` (`dict_id`, `tenant_id`, `dict_name`, `dict_type`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1851976902360236033, '000000', '用户类型', 'user_type', 103, 1, '2024-10-31 21:17:56', 1, '2024-10-31 21:17:56', 'pc管理端用戶、客戶端用戶');
+
+-- 字典数据表
+INSERT INTO `sys_dict_data` (`dict_code`, `tenant_id`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1851635425633472514, '000000', 0, 'PC客户端', 'pc_client', 'sys_device_type', '', 'default', 'N', 103, 1, '2024-10-30 22:41:02', 1, '2024-10-30 22:41:02', 'PC客户端');
+INSERT INTO `sys_dict_data` (`dict_code`, `tenant_id`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1851901153796288514, '000000', 0, '客户端密码认证', 'clientPassword', 'sys_grant_type', 'el-check-tag', 'default', 'N', 103, 1, '2024-10-31 16:16:56', 1, '2024-10-31 16:17:36', '客户端密码认证');
+INSERT INTO `sys_dict_data` (`dict_code`, `tenant_id`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1851977152646938626, '000000', 0, 'PC管理端用戶', 'sys_user', 'user_type', '', 'default', 'N', 103, 1, '2024-10-31 21:18:56', 1, '2024-10-31 21:18:56', 'PC管理端用戶');
+INSERT INTO `sys_dict_data` (`dict_code`, `tenant_id`, `dict_sort`, `dict_label`, `dict_value`, `dict_type`, `css_class`, `list_class`, `is_default`, `create_dept`, `create_by`, `create_time`, `update_by`, `update_time`, `remark`) VALUES (1851977265763123201, '000000', 0, '客戶端用戶', 'cli_user', 'user_type', '', 'default', 'N', 103, 1, '2024-10-31 21:19:23', 1, '2024-10-31 21:19:23', '此用户可以是PC客户端、APP客户端、小程序客户端等');
