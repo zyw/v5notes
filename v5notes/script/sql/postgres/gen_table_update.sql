8538dcdb5abfb42d67f2b0475aa464009bf8cab5
zyw <zyw090111@163.com>
2025-10-01 15:24:54 +0800

    feat(gen): 添加后端代码类型和前端模版名称字典数据- 在 gen_table_column 表中新增 size 和 default_val 字段 - 插入后端代码类型字典类型 gen_be_type 及其数据（Java、Golang）- 插入前端模版名称字典类型 gen_fe_type 及其数据（默认、Soybean Naive UI、Soybean Antd） - 支持生成不同后端语言和前端框架的代码模板配置

--- v5notes/script/sql/postgres/gen_table_update.sql
+++ v5notes/script/sql/postgres/gen_table_update.sql
@@ -27,3 +27,22 @@ COMMENT ON COLUMN gen_table_column.default_val IS '默认值';
 -- 添加字段长度字段
 ALTER TABLE gen_table_column ADD COLUMN size VARCHAR(20);
 COMMENT ON COLUMN gen_table_column.size IS '字段长度';
+
+
+-- 添加后端代码类型字典表
+INSERT INTO sys_dict_type (dict_id, tenant_id, dict_name, dict_type, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (114, '000000', '后端代码类型', 'gen_be_type', 100, 1, '2025-09-28 15:18:28', 1, '2025-09-28 15:18:28', '生成的后端代码类型，例如：java,golang');
+INSERT INTO sys_dict_type (dict_id, tenant_id, dict_name, dict_type, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (115, '000000', '前端模版名称', 'gen_fe_type', 100, 1, '2025-09-28 15:20:47', 1, '2025-09-28 15:20:47', '要生成的前端模版目录名称');
+
+-- 添加后端代码类型数据
+INSERT INTO sys_dict_data (dict_code, tenant_id, dict_sort, dict_label, dict_value, dict_type, css_class, list_class, is_default, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (144, '000000', 2, 'Golang', 'golang', 'gen_be_type', '', 'default', 'N', 100, 1, '2025-09-28 15:19:15', 1, '2025-09-28 15:19:15', 'golang语言');
+INSERT INTO sys_dict_data (dict_code, tenant_id, dict_sort, dict_label, dict_value, dict_type, css_class, list_class, is_default, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (145, '000000', 1, 'Java', 'java', 'gen_be_type', '', 'default', 'N', 100, 1, '2025-09-28 15:19:38', 1, '2025-09-28 15:19:38', 'Java语言');
+INSERT INTO sys_dict_data (dict_code, tenant_id, dict_sort, dict_label, dict_value, dict_type, css_class, list_class, is_default, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (146, '000000', 0, '默认', 'default', 'gen_fe_type', '', 'default', 'N', 100, 1, '2025-09-28 15:21:38', 1, '2025-09-28 15:21:38', '默认模版');
+INSERT INTO sys_dict_data (dict_code, tenant_id, dict_sort, dict_label, dict_value, dict_type, css_class, list_class, is_default, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (147, '000000', 2, 'Soybean Naive UI', 'soy-naive', 'gen_fe_type', '', 'default', 'N', 100, 1, '2025-09-28 15:22:36', 1, '2025-09-28 15:22:36', 'Soybean Naive UI');
+INSERT INTO sys_dict_data (dict_code, tenant_id, dict_sort, dict_label, dict_value, dict_type, css_class, list_class, is_default, create_dept, create_by, create_time, update_by, update_time, remark)
+                   VALUES (148, '000000', 1, 'Soybean Antd', 'soy-antd', 'gen_fe_type', '', 'default', 'N', 100, 1, '2025-09-28 15:25:06', 1, '2025-09-28 15:25:06', 'Soybean Ant Design Vue');
